b7031c1fa0b9fc1c4894fedd2fd08c8615fdeb30
Abram <tacksontiger@gmail.com>
2025-09-17 18:39:35 +0300

    commit #2
    Added JPanel and cardLayout, then Initialize them.

--- src/MenuApp.java
+++ src/MenuApp.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MenuApp {
     JFrame frame;
+    JPanel cardPanel;
+    CardLayout cardLayout;
     MenuApp() {
         frame = new JFrame("Menu Example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
+
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
     }
